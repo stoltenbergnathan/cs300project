57e58a75b2910f8c3b2f62e1aa3377bc96bbb1ed
Nathan <nds4@pdx.edu>
2021-04-29 10:40:43 -0700

    New

--- tests/mockAPI.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-//call login api to get all users
-async function getFirstUserName() {
-  const response = await axios.get('http://localhost:6978/user/login');
-  return response.data[0].name;
-}
-
-module.exports = getFirstUserName;
